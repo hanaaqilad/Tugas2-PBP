8f774c228a34574e8a52bfbe86fee4d50d5d5b5a
Hana <hana@Hanas-MacBook-Pro.local>
2022-09-12 19:41:22 +0700

    deploy tugas 2 pbp

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    data_catalog_item = CatalogItem.objects.all()
+    context = {
+        'name': 'Hana Devi Aqila',
+        'student_id': '2106751556',
+        'list_item': data_catalog_item,
+    }
+
+    return render(request, "katalog.html", context)
