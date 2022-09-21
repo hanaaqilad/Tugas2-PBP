d816bb93f6aee0d6a5cad85100e998544fd7e407
Hana <hana@Hanas-MacBook-Pro.local>
2022-09-21 18:05:28 +0700

    update test

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,3 +1,34 @@
-from django.test import TestCase
+from audioop import reverse
+from django.test import TestCase, Client
+from django.urls import resolve
 
 # Create your tests here.
+class Test(TestCase):
+    def test_html(self):
+        response = Client().get('/mywatchlist/html/')
+        self.assertEqual(response.status_code,200)
+
+    def test_xml(self):
+        response = Client().get('/mywatchlist/xml/')
+        self.assertEqual(response.status_code,200)
+
+    def test_json(self):
+        response = Client().get('/mywatchlist/json/')
+        self.assertEqual(response.status_code,200)
+
+    # def test_html(self):
+    #     client = Client()
+    #     response = client.get(reverse("mywatchlist:show_html"))
+    #     self.assertEqual(response.status_code,200)
+
+    # def test_xml(self):
+    #     client = Client()
+    #     response = client.get(reverse("MyWatchList:show_xml"))
+    #     self.assertEqual(response.status_code,200)
+
+    # def test_json(self):
+    #     client = Client()
+    #     response = client.get(reverse("mywatchlist:show_json"))
+    #     self.assertEqual(response.status_code,200)
+
+    
